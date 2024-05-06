e85aa0f9de4cd6604a7a25c9b75b95ab08896eab
Hakag3 <wolfex2016@yandex.ru>
2024-05-06 16:53:14 +0300

    alfa 2 opt mobile
    release mobile navigation

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 
 
 
-let mySlider = new Splide( '#mySlider', {
-    direction: 'ttb',
-    width  : '100%',
-    height : '65vh',
-    wheel    : true,
-  } );
-  
-mySlider.mount();
+// let mySlider = new Splide( '#mySlider', {
+//     direction: 'ttb',
+//     width  : '100%',
+//     height : '65vh',
+//     wheel    : true,
+//   } );
 
+// mySlider.mount();
+ 
+document.addEventListener("DOMContentLoaded",function(){
+  document.getElementById("burger").addEventListener("click", function(){
+    document.querySelector(".header_nav").classList.toggle("open")
+  })
+})
