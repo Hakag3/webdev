47e7d579f3b2737a14df3affe3624ab957a52658
Hakag3 <wolfex2016@yandex.ru>
2024-05-06 13:17:28 +0300

    alfa 1 opt mobile

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
 
 let mySlider = new Splide( '#mySlider', {
     direction: 'ttb',
-    height   : '900px',
+    width  : '100%',
+    height : '65vh',
     wheel    : true,
   } );
   
 mySlider.mount();
+
