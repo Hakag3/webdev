97702d7011cf632c83fdc9a6d31a96ee69ec9d76
Hakag3 <wolfex2016@yandex.ru>
2024-05-07 19:15:14 +0300

    import swiper

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
 
 
 
-let mySlider = new Splide( '#mySlider', {
-    direction: 'ttb',
-    width  : '100%',
-    height : '65vh',
-    wheel    : true,
-  } );
 
-mySlider.mount();
- 
-document.addEventListener("DOMContentLoaded",function(){
-  document.getElementById("burger").addEventListener("click", function(){
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("burger").addEventListener("click", function () {
     document.querySelector(".header_nav").classList.toggle("open")
   })
 })
+
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: false,
+  speed: 1000,
+  direction: "vertical",
+  slidesPerView: "auto",
+ 
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+   
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
