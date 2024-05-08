1cf6338e78163e5d6898f033b98506697800807b
Hakag3 <wolfex2016@yandex.ru>
2024-05-08 11:58:14 +0300

    opt mobile fixed 2

--- main.js
+++ main.js
@@ -4,35 +4,19 @@
 
 
 
+
+
+
+let swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("burger").addEventListener("click", function () {
     document.querySelector(".header_nav").classList.toggle("open")
   })
 })
-
-
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  loop: false,
-  speed: 1000,
-  direction: "vertical",
-  slidesPerView: "auto",
- 
-  
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-   
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-
-});
